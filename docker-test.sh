62ac524f646ddc4cd8e0d7f97acf8007df4f1fa8
Tyler Hoffman <tyler@memfault.com>
2020-07-06 23:03:16 -0700

    Enable tests to run locally

--- docker-test.sh
+++ docker-test.sh
@@ -10,13 +10,13 @@
 # First argument is the ELF location.
 
 declare -r HOST_ROOT_DIR=`pwd`
-declare -r HOST_TEST_OUTPUT_PATH=${HOST_ROOT_DIR}/test_output
-declare -r HOST_LOG_PATH=${HOST_TEST_OUTPUT_PATH}
+declare -r HOST_TEST_RESULTS_PATH=${HOST_ROOT_DIR}/test_results
+declare -r HOST_LOG_PATH=${HOST_TEST_RESULTS_PATH}
 declare -r HOST_LOG_FILENAME=${HOST_LOG_PATH}/logs.txt
 
 declare -r DOCKER_TAG=renode_stm32f4
 declare -r DOCKER_WORKSPACE=/workspace
-declare -r DOCKER_TEST_OUTPUT_PATH=/tmp/test_output
+declare -r DOCKER_TEST_RESULTS_PATH=/tmp/test_results
 
 mkdir -p ${HOST_LOG_PATH}
 
@@ -28,12 +28,12 @@ exit_code=0
 if ! docker run \
   --log-driver=none -a stdout -a stderr \
   --volume ${HOST_ROOT_DIR}:${DOCKER_WORKSPACE} \
-  --volume ${HOST_TEST_OUTPUT_PATH}:${DOCKER_TEST_OUTPUT_PATH} \
+  --volume ${HOST_TEST_RESULTS_PATH}:${DOCKER_TEST_RESULTS_PATH} \
   --env SCRIPT=${DOCKER_WORKSPACE}/renode-config.resc \
   --env RENODE_CHECKOUT=/home/developer/renode \
   --workdir ${DOCKER_WORKSPACE} \
   ${DOCKER_TAG} \
-  /bin/bash -c "./run_tests.sh 2>&1 > ${DOCKER_TEST_OUTPUT_PATH}/logs.txt"
+  /bin/bash -c "./run_tests.sh 2>&1 > ${DOCKER_TEST_RESULTS_PATH}/logs.txt"
 then
   echo "FAILED"
   exit_code=1
--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 RENODE_CHECKOUT=${RENODE_CHECKOUT:-~/code/renode}
 
-${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_output"
+${RENODE_CHECKOUT}/test.sh -t "${PWD}/tests/tests.yaml" --variable PWD_PATH:"${PWD}" -r "${PWD}/test_results"
